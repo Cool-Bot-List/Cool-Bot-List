615b68cd2fac3a4f82f6bc09cf57d056f402177f
creeperplanet26 <fortniteplanet26>
2020-07-18 19:16:09 -0400

    fixed an issue where an error would always happen

--- server/api/bots.js
+++ server/api/bots.js
@@ -6,10 +6,23 @@ const Bot = require("../database/models/Bot.js");
 //Need a good error codes for all of these. Please help
 
 router.post("/", async (req, res) => {
-// eslint-disable-next-line no-unused-vars
+
+    router.get("/:id", async (req, res) => {
+        const { id } = req.params;
+        if (!id) { 
+            //gets all bots if no id
+            const allBots = await Bots.find();
+            return res.json(allBots); 
+        }
+        //gets 1 bot if id
+        const foundBot = await Bots.findOne({ id });
+        if (!foundBot) return res.status(404).json({ message: "A bot was not found!" });
+        return res.json(foundBot);
+    });
+ 
     const { id, name, prefix, description, owners, website, helpCommand, supportServer, library } = req.body;
-    if (id || name || prefix || description || owners || website || helpCommand ||
-        supportServer || library) return res.status(404).json({ msg: "Your missing some information to create the bot!" });
+    if (!id || !name || !prefix || !description || !owners || !website || !helpCommand ||
+        !supportServer || !library) return res.status(404).json({ msg: "Your missing some information to create the bot!" });
     const bot = await Bots.findOne({ id });
     //need a good error code
     if (bot) return res.status(404).send({ message: "This bot already exists!" }); 
@@ -23,18 +36,6 @@ router.post("/", async (req, res) => {
     return res.json({ message: "Succesfully created a new bot in the database!" });
 });
 
-router.get("/:id", async (req, res) => {
-    const { id } = req.params;
-    if (!id) { 
-        //gets all bots if no id
-        const allBots = await Bots.find();
-        return res.json(allBots); 
-    }
-    //gets 1 bot if id
-    const foundBot = await Bots.findOne({ id });
-    if (!foundBot) return res.status(404).json({ message: "A bot was not found!" });
-    return res.json(foundBot);
-});
 
 router.put("/", async (req, res ) => { 
     const foundBot = await Bots.findOneAndUpdate( req.body.id, req.body, { new: true});
@@ -58,8 +59,6 @@ router.delete("/:id", async (req, res) => {
 }); 
 module.exports = router;
 
-// Couldn't post
-// get or post make a post with all the feilds ok
-// use localhost:5000/api
+//https://github.com/Zyleaf/Cool-Bot-List/pull/2 for all the error codes
 // ill look up some status stuff and commit in the mean time
 
