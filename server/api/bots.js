9772c713b6067f127e40b4a1a4ed5108394e3a05
creeperplanet26 <fortniteplanet26>
2020-07-18 14:57:31 -0400

    Finished eslint setup

--- server/api/bots.js
+++ server/api/bots.js
@@ -1,12 +1,13 @@
-const express = require("express");
-const router = express.Router();
-const Bots = require("../database/models/Bot.js");
+const express = require('express')
+const router = express.Router()
+const Bots = require('../database/models/Bot.js')
 
-router.post("/", async (req, res) => {
-  const { ID, NAME, PREFIX, DESCRIPTION, OWNERS, WEBSITE, HELP_COMMAND, SUPPORT_SERVER, LIBRARY } = req.body;
-  const bot = await Bots.findOne({ ID });
-  //need a good error code
-  if (bot) return res.status(404).send({ message: "This bot already exists!" });
-});
+router.post('/', async (req, res) => {
+	// eslint-disable-next-line no-unused-vars
+	const { ID, NAME, PREFIX, DESCRIPTION, OWNERS, WEBSITE, HELP_COMMAND, SUPPORT_SERVER, LIBRARY } = req.body
+	const bot = await Bots.findOne({ ID })
+	//need a good error code
+	if (bot) return res.status(404).send({ message: 'This bot already exists!' })
+})
 
-module.exports = router;
+module.exports = router
