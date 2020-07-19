a37402a491dc7582a75278e88abd2b3d0759d55d
creeperplanet26 <fortniteplanet26>
2020-07-19 19:39:39 -0400

    Linted files

--- server/api/users.js
+++ server/api/users.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const User = require('../database/models/User.js');
+const User = require("../database/models/User.js");
 
 // post user
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
 
     const { id, bio, bots } = req.body;
     if (!id || !bio || !bots) return res.status(403).json({ message: "You are missing paramaters" });
@@ -15,11 +15,11 @@ router.post('/', async (req, res) => {
     } catch (err) { //     :: I have no idea what status code to put here, someone help lmao
         return res.status(404).json({ message: "Something went wrong and the user was not saved to the database" });
     }
-    return res.status(200).json({ message: "Successfully created a new user and added them to the database" })
+    return res.status(200).json({ message: "Successfully created a new user and added them to the database" });
 });
 
 // get specific user
-router.get('/:id', async (req, res) => {
+router.get("/:id", async (req, res) => {
 
     const { id } = req.params;
     const user = await User.findOne({ id });
@@ -29,19 +29,19 @@ router.get('/:id', async (req, res) => {
 });
 
 // all users ::
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
     const users = await User.find();
     if (!users) return res.status(404).json({ message: "There are no users in the database!" });
     res.status(200).json(users);
 });
 
 // update user
-router.put('/', async (req, res) => {
+router.put("/", async (req, res) => {
     
 });
 
 // delete user
-router.delete('/:id', async (req, res) => {
+router.delete("/:id", async (req, res) => {
 
 });
 
--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-require('./database/database');
+require("./database/database");
 
 
 app.use(express.json());
@@ -8,8 +8,8 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json(), express.urlencoded({ extended: true }));
 
-app.use('/api/bots', require('./api/bots'));
-app.use('/api/users', require('./api/users'));
+app.use("/api/bots", require("./api/bots"));
+app.use("/api/users", require("./api/users"));
 //semi colons smh
 const PORT = 5000;
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
