b555be45b76b87d106761eb4ad53c135a2df74da
creeperplanet26 <fortniteplanet26>
2020-07-19 19:54:09 -0400

    updated error codes and messages for user route

--- server/api/users.js
+++ server/api/users.js
@@ -6,14 +6,14 @@ const User = require("../database/models/User.js");
 router.post("/", async (req, res) => {
 
     const { id, bio, bots } = req.body;
-    if (!id || !bio || !bots) return res.status(403).json({ message: "You are missing paramaters" });
+    if (!id || !bio || !bots) return res.status(400).json({ message: "You are missing paramaters", error: "Bad Request." });
     const user = await User.findOne({ id });
     if (user) return res.status(430).json({ message: "That user already exists" });
     const newUser = new User({ id, bio, bots });
     try {
         await newUser.save();
     } catch (err) { //     :: I have no idea what status code to put here, someone help lmao
-        return res.status(404).json({ message: "Something went wrong and the user was not saved to the database" });
+        return res.status(500).json({ message: "Something went wrong and the user was not saved to the database", error: "Internal Server Error." });
     }
     return res.status(200).json({ message: "Successfully created a new user and added them to the database" });
 });
@@ -23,7 +23,7 @@ router.get("/:id", async (req, res) => {
 
     const { id } = req.params;
     const user = await User.findOne({ id });
-    if (!user) return res.status(404).json({ message: "That user doesn't exist!" });
+    if (!user) return res.status(404).json({ message: "That user doesn't exist!", error: "Not Found." });
     res.status(200).json({ user });
 
 });
@@ -31,13 +31,13 @@ router.get("/:id", async (req, res) => {
 // all users ::
 router.get("/", async (req, res) => {
     const users = await User.find();
-    if (!users) return res.status(404).json({ message: "There are no users in the database!" });
+    if (!users) return res.status(404).json({ message: "There are no users in the database!", error:"Not Found." });
     res.status(200).json(users);
 });
 
 // update user
 router.put("/", async (req, res) => {
-    
+
 });
 
 // delete user
