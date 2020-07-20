75db93ac7e65a86326f97d9c325cc9954eeade27
creeperplanet26 <fortniteplanet26>
2020-07-20 17:19:55 -0400

    made it not required and set a default

--- server/database/models/Review.js
+++ server/database/models/Review.js
@@ -8,7 +8,7 @@ const Review = new mongoose.Schema({
     // reply: String, -- Removed for now
     likes: Number,
     dislikes: Number,
-    rating: Number,
+    rating: { type: Number, default: 0, required: false },
 });
 
 module.exports = mongoose.model("reviews", Review);
