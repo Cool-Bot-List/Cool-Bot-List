f48aef9073c901a66c0566dd20b9885ddb884393
creeperplanet26 <fortniteplanet26>
2020-07-20 14:34:26 -0400

    fixed where it was ID sometimes instead of id

--- server/api/reviews.js
+++ server/api/reviews.js
@@ -8,17 +8,17 @@ const Review = require("../database/models/Review");
 router.post("/reviews/:id", async (req, res) => {
 
     const { id } = req.params;
-    const { userID, review } = req.body;
+    const { userId, review } = req.body;
 
     // Check if the bot exists
     const foundBot = await Bots.findOne({ id });
     if (!foundBot) return res.status(404).json({ message: "That bot doesn't exist in the database.", error: "Not Found."});
 
     // Check if the user already reviewed this bot
-    const userReviewd = await Reviews.findOne({ botId: id, userID });
+    const userReviewd = await Reviews.findOne({ botId: id, userId });
     if (userReviewd) return res.status(400).json({ message: "You already reviewed this bot.", error: "Bad Request." });
 
-    const newReview = new Reviews({ botId: id, userID, review, reply: "", likes: 0, dislikes: 0 });
+    const newReview = new Reviews({ botId: id, userId, review, reply: "", likes: 0, dislikes: 0 });
     try {
         newReview.save();
     } catch (err) {
