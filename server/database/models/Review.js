f178599afd02e94483d3ee7cc8fbb3265139c600
creeperplanet26 <fortniteplanet26>
2020-07-20 17:38:34 -0400

    changed the ratting to number + added example object

--- server/database/models/Review.js
+++ server/database/models/Review.js
@@ -8,7 +8,17 @@ const Review = new mongoose.Schema({
     // reply: String, -- Removed for now
     likes: Number,
     dislikes: Number,
-    ratings: { type: Array, default: [], required: false },
+    ratings: { type: Number, default: null, required: false }, //should this be stars instead
 });
+// example object
+const example = {
+    botId: "23984928439238492",
+    userId: "1342358234958",
+    review: "idk what this even is tbh",
+    // reply: String, -- Removed for now
+    likes: 1,
+    dislikes: 2,
+    ratings: 4,
+};
 
 module.exports = mongoose.model("reviews", Review);
