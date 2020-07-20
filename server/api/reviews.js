07e5c2a9b546381c02589aefcfe3d1420351d31f
Maxisthemoose <oriontothemax@gmail.com>
2020-07-19 21:47:50 -0700

    Added checks - User reviewed already + Bot exists

--- server/api/reviews.js
+++ server/api/reviews.js
@@ -1,13 +1,22 @@
 const express = require("express");
 const router = express.Router();
 const Reviews = require("../database/models/Review");
-
+const Bots = require("../database/models/Bot");
 
 // Post user review -- requires Oauth to actually function --
 router.post("/reviews/:id", async (req, res) => {
 
     const { id } = req.params;
     const { userID, review } = req.body;
+
+    // Check if the bot exists
+    const foundBot = await Bots.findOne({ id });
+    if (!foundBot) return res.status(404).json({ message: "That bot doesn't exist in the database.", error: "Not Found."});
+
+    // Check if the user already reviewed this bot
+    const userReviewd = await Reviews.findOne({ botID: id, userID });
+    if (userReviewd) return res.status(400).json({ message: "You already reviewed this bot.", error: "Bad Request." });
+
     const newReview = new Reviews({ botID: id, userID, review, reply: "", likes: 0, dislikes: 0 });
     try {
         newReview.save();
