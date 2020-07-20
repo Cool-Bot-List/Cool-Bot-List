c8edeaa0bf517d4dc697e5bd807b7f30176b5662
creeperplanet26 <fortniteplanet26>
2020-07-19 20:34:45 -0400

    finished DELETE api/users/:id

--- server/api/users.js
+++ server/api/users.js
@@ -4,7 +4,6 @@ const User = require("../database/models/User.js");
 
 // post user
 router.post("/", async (req, res) => {
-
     const { id, bio, bots } = req.body;
     if (!id || !bio || !bots) return res.status(400).json({ message: "You are missing paramaters", error: "Bad Request." });
     const user = await User.findOne({ id });
@@ -12,7 +11,8 @@ router.post("/", async (req, res) => {
     const newUser = new User({ id, bio, bots });
     try {
         await newUser.save();
-    } catch (err) { //     :: I have no idea what status code to put here, someone help lmao
+    } catch (err) {
+    //     :: I have no idea what status code to put here, someone help lmao
         return res.status(500).json({ message: "Something went wrong and the user was not saved to the database", error: "Internal Server Error." });
     }
     return res.status(200).json({ message: "Successfully created a new user and added them to the database" });
@@ -20,18 +20,16 @@ router.post("/", async (req, res) => {
 
 // get specific user
 router.get("/:id", async (req, res) => {
-
     const { id } = req.params;
     const user = await User.findOne({ id });
     if (!user) return res.status(404).json({ message: "That user doesn't exist!", error: "Not Found." });
     res.status(200).json({ user });
-
 });
 
 // all users ::
 router.get("/", async (req, res) => {
     const users = await User.find();
-    if (!users) return res.status(404).json({ message: "There are no users in the database!", error:"Not Found." });
+    if (!users) return res.status(404).json({ message: "There are no users in the database!", error: "Not Found." });
     res.status(200).json(users);
 });
 
@@ -39,8 +37,8 @@ router.get("/", async (req, res) => {
 router.put("/", async (req, res) => {
     const { id, bio, bots } = req.body;
     if (!id || !bio || !bots) return res.status(400).json({ message: "You are missing paramaters", error: "Bad Request." });
-    const foundUser = await User.findOneAndUpdate( req.body.id, req.body, { new: true});
-    try { 
+    const foundUser = await User.findOneAndUpdate(req.body.id, req.body, { new: true });
+    try {
         await foundUser.save();
     } catch (err) {
         return res.status(500).json({ message: "Something went wrong and the bot did not save to the database!" });
@@ -50,7 +48,16 @@ router.put("/", async (req, res) => {
 
 // delete user
 router.delete("/:id", async (req, res) => {
-
+    const { id } = req.params;
+    if (!id) return res.status(400).json({ message: "You are missing paramaters", error: "Bad Request." });
+    const foundUser = await User.findOne({ id });
+    if (!foundUser) return res.status(404).json({ message: "That user doesn't exist in the database!" });
+    try {
+        await User.findByIdAndDelete({ id });
+    } catch (err) {
+        return res.status(500).json({ message: "Something went wrong and the user did not delete from the database!", error: "Internal Server Error." }); 
+    }
+    return res.json({ message: "Succesfully deleted the user from the database!" });
 });
 
 module.exports = router;
