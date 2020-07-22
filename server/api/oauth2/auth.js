3489a100814761afa1e2fab71be3a2cc58f18fc6
creeperplanet26 <fortniteplanet26>
2020-07-22 14:01:16 -0400

    added better naming

--- server/api/oauth2/auth.js
+++ server/api/oauth2/auth.js
@@ -3,10 +3,12 @@ const passport = require("passport");
 
 router.get("/login", passport.authenticate("discord"));
 router.get("/login/redirect", passport.authenticate("discord"), (req, res) => {
-    if (req.user.askedBioQueston) {
+    if (req.user.isNew) {
+        res.redirect("http://localhost:3000/bio/add");
+    } else if (!req.user.isNew) {
         res.redirect(`http://localhost:3000/users/${req.user.id}`);
     } else {
-        res.redirect("http://localhost:3000/bio/add");
+        res.redirect("http://localhost:3000");
     }
     res.status(200).json({ message: "Yes" });
 });
