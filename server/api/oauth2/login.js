a4f28871b72ba565c5e4830abc9416847a25d3e8
creeperplanet26 <fortniteplanet26>
2020-07-22 14:17:47 -0400

    changed the user to not new after they are redirected

--- server/api/oauth2/login.js
+++ server/api/oauth2/login.js
@@ -1,10 +1,13 @@
 const router = require("express").Router();
 const passport = require("passport");
+const Users = require("../../database/models/User");
 
 router.get("/login", passport.authenticate("discord"));
-router.get("/login/redirect", passport.authenticate("discord"), (req, res) => {
+router.get("/login/redirect", passport.authenticate("discord"), async (req, res) => {
     if (req.user.isNew) {
         res.redirect("http://localhost:3000/bio/add");
+        const user = await Users.findOne({ id: req.user.id });
+        user.isNew = false;
     } else if (!req.user.isNew) {
         res.redirect(`http://localhost:3000/users/${req.user.id}`);
     } else {
