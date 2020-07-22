4b170c99c0de629e7115a004fdc04243d08db09e
creeperplanet26 <fortniteplanet26>
2020-07-22 14:04:24 -0400

    Added a feature where the users data gets updated on login

--- server/api/oauth2/strategies/discordStrategy.js
+++ server/api/oauth2/strategies/discordStrategy.js
@@ -11,28 +11,37 @@ passport.deserializeUser(async (id, done) => {
     if (user) done(null, user);
 });
 
-passport.use(new DiscordStrategy({
-    clientID: "735273348875026573",
-    clientSecret: "YLj5rbnzhIuV1seAQ7VeitS1pSLkGKW7",
-    callbackURL: "http://localhost:5000/api/login/redirect",
-    scope: ["identify", "applications.builds.read"],
-}, async (accesToken, refreshToken, profile, done) => {
-    try {
-        const user = await User.findOne({ id: profile.id });
-        if (user) {
-            done(null, user);
-        } else {
-            const newUser = new User({ 
-                id: profile.id, 
-                discriminator: profile.discriminator,
-                username: profile.username,
-                avatarUrl: `https://cdn.discordapp.com/avatars/${profile.id}/${profile.avatar}.png?size=2048`,
-            });
-            const savedUser = await newUser.save();
-            done(null, savedUser);
+passport.use(
+    new DiscordStrategy(
+        {
+            clientID: "735273348875026573",
+            clientSecret: "YLj5rbnzhIuV1seAQ7VeitS1pSLkGKW7",
+            callbackURL: "http://localhost:5000/api/login/redirect",
+            scope: ["identify", "applications.builds.read"],
+        },
+        async (accesToken, refreshToken, profile, done) => {
+            try {
+                const user = await User.findOne({ id: profile.id });
+                if (user) {
+                    user.avatarUrl = `https://cdn.discordapp.com/avatars/${profile.id}/${profile.avatar}.png?size=2048`;
+                    user.username = profile.username;
+                    user.discriminator = profile.discriminator;
+                    await user.save();
+                    done(null, user);
+                } else {
+                    const newUser = new User({
+                        id: profile.id,
+                        discriminator: profile.discriminator,
+                        username: profile.username,
+                        avatarUrl: `https://cdn.discordapp.com/avatars/${profile.id}/${profile.avatar}.png?size=2048`,
+                    });
+                    const savedUser = await newUser.save();
+                    done(null, savedUser);
+                }
+            } catch (err) {
+                console.log(err);
+                done(err, null);
+            }
         }
-    } catch (err) {
-        console.log(err);
-        done(err, null);
-    }
-}));
+    )
+);
