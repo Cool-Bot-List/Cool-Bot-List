d9d143937b01a84c992e0d299f78ea446c3640bd
creeperplanet26 <fortniteplanet26>
2020-07-22 13:57:42 -0400

    Redirected the user based on wherter they added a bio

--- server/api/oauth2/auth.js
+++ server/api/oauth2/auth.js
@@ -3,8 +3,12 @@ const passport = require("passport");
 
 router.get("/login", passport.authenticate("discord"));
 router.get("/login/redirect", passport.authenticate("discord"), (req, res) => {
+    if (req.user.askedBioQueston) {
+        res.redirect(`http://localhost:3000/users/${req.user.id}`);
+    } else {
+        res.redirect("http://localhost:3000/bio/add");
+    }
     res.status(200).json({ message: "Yes" });
 });
-
 
 module.exports = router;
--- server/database/models/User.js
+++ server/database/models/User.js
@@ -8,6 +8,7 @@ const User = new mongoose.Schema({
     avatarUrl: { type: String, default: "https://discordapp.com/assets/322c936a8c8be1b803cd94861bdfa868.png", required: false },
     bio: { type: String, default: "", required: false },
     bots: { type: Array, default: [], required: false },
+    askedBioQueston: { type: Boolean, default: false, required: false },
 });
 
 module.exports = mongoose.model("users", User);
