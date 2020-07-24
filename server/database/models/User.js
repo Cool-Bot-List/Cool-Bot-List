8078b3d48f82b544ecb45d71621d300d4f6daaf4
creeperplanet26 <fortniteplanet26>
2020-07-24 14:17:25 -0400

    made the id unique

--- server/database/models/User.js
+++ server/database/models/User.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 mongoose.pluralize(null);
 
 const User = new mongoose.Schema({
-    id: { type: String, required: true },
+    id: { type: String, required: true, unique: true },
     tag: { type: String, required: true },
     avatarUrl: { type: String, default: "https://discordapp.com/assets/322c936a8c8be1b803cd94861bdfa868.png", required: false },
     bio: { type: String, default: "", required: false },
