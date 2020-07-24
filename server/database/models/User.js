857d22d04f5a117a40a20c4cc76b1bf412433176
creeperplanet26 <fortniteplanet26>
2020-07-24 14:13:27 -0400

    updated schema to remove username and descrimintator and add tag instead

--- server/database/models/User.js
+++ server/database/models/User.js
@@ -3,8 +3,7 @@ mongoose.pluralize(null);
 
 const User = new mongoose.Schema({
     id: { type: String, required: true },
-    discriminator: { type: String, required: true },
-    username: { type: String, required: true },
+    tag: { type: String, required: true },
     avatarUrl: { type: String, default: "https://discordapp.com/assets/322c936a8c8be1b803cd94861bdfa868.png", required: false },
     bio: { type: String, default: "", required: false },
     bots: { type: Array, default: [], required: false },
