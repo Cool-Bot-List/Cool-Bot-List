e5f1a718e13e8cf69b9263acaca3ad9383eb355e
CreeperPlanet26 <fortniteplanet26@gmail.com>
2020-08-02 20:28:54 -0400

    started  jsdoc for getBotData util fn

--- server/util/getBotData.js
+++ server/util/getBotData.js
@@ -5,6 +5,13 @@ const axios = require("axios").default;
 
 axios.defaults.headers.common["Authorization"] = `Bot ${process.env.BOT_TOKEN}`;
 
+/**
+ * Get the tag and avatar url of a bot based on its id.
+ * @param {string} botId - The discord id of the bot.
+ * @type {object} data - Returns an object with the bot tag and avatar url
+ * @property {string} data.tag - the tag of the bot
+ * @returns {object} a
+ */
 const getBotData = async (botId) => {
     const response = await axios.get(`https://discord.com/api/v6/users/${botId}`);
     const avatarUrl = getAvatarUrl(botId, response.data.avatar);
