7204cba0f643a0e16df0f60b374bc8bb1f5055d4
CreeperPlanet26 <fortniteplanet26@gmail.com>
2020-08-06 15:38:16 -0400

    boilerplate for creating a token for user. still need logic to create the token

--- server/api/token.js
+++ server/api/token.js
@@ -11,5 +11,17 @@ router.get("/:id", async (req, res) => {
     if (!foundUser.token) return res.status(404).json({ message: "This user doesn't have a token!", error: "Not Found" });
     res.status(200).json(foundUser.token);
 });
+// Create the token for an user.
+router.post("/:id", async (req, res) => {
+    const { id } = req.params;
+    if (!id) return res.status(400).json({ message: "You are missing the id of the user to update in your params.", error: "Bad Request." });
+    const foundUser = Users.findOne({ id });
+    if (!id) return res.status(404).json({ message: "A user was not found", error: "Not Found." });
+
+    // Logic to create the user's token here
+
+    WebSocket.emit("new-token", foundUser);
+    return res.status(201).json({ message: "Successfully created the user a token." });
+});
 
 module.exports = router;
