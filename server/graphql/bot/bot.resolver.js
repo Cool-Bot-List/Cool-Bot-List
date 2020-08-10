799d34978910f22c2b8624cf567146abb7a665c5
CreeperPlanet26 <fortniteplanet26@gmail.com>
2020-08-10 19:33:53 -0400

    get all Bots works

--- server/graphql/bot/bot.resolver.js
+++ server/graphql/bot/bot.resolver.js
@@ -3,6 +3,7 @@ const Bots = require("../../database/models/Bot");
 const BotResolver = {
     Query: {
         Bot: async (_parent, { id }) => await Bots.findOne({ id }),
+        Bots: async () => await Bots.find(),
     },
 };
 
--- server/graphql/queries.js
+++ server/graphql/queries.js
@@ -3,6 +3,7 @@ const { gql } = require("apollo-server-express");
 const Queries = gql`
     type Query {
         Bot(id: ID!): Bot
+        Bots: [Bot]
     }
 `;
 
