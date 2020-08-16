adaba050a8322736bc8638cf7d660fdf43411a05
CreeperPlanet26 <fortniteplanet26@gmail.com>
2020-08-16 19:17:21 -0400

    started ownerReply create

--- server/graphql/ownerReply/ownerReply.type.ts
+++ server/graphql/ownerReply/ownerReply.type.ts
@@ -2,6 +2,10 @@ import { gql } from "apollo-server-express";
 
 const OwnerReplyType = gql`
     type OwnerReply {
+        # Create an ownerReply
+        create(data: OwnerReplyCreatable)
+
+
         # The userId of the returned Review.
         userId: String
         # The user object of the returned Review.
@@ -10,6 +14,10 @@ const OwnerReplyType = gql`
         likes: [String!]!
         dislikes: [String!]!
         date: String
+    }
+    type OwnerReplyCreatable {
+        userId: String!
+        review: String!
     }
 `;
 
