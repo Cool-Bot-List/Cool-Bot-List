5594f59ee37e1af5e113654c78721617ef246f4c
CreeperPlanet26 <fortniteplanet26@gmail.com>
2020-08-25 18:17:33 -0400

    setup graphql and mongoose

--- nest/src/app.module.ts
+++ nest/src/app.module.ts
@@ -1,6 +1,13 @@
 import { Module } from "@nestjs/common";
+import { GraphQLModule } from "@nestjs/graphql";
+import { MongooseModule } from "@nestjs/mongoose";
 
 @Module({
-    imports: [],
+    imports: [
+        GraphQLModule.forRoot({
+            autoSchemaFile: "src/schema.gql",
+        }),
+        MongooseModule.forRoot(process.env.MONGO_URI),
+    ],
 })
 export class AppModule { }
--- nest/src/main.ts
+++ nest/src/main.ts
@@ -1,3 +1,4 @@
+import { config } from "dotenv"; config();
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 
