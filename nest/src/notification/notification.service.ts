0a66c98aa98ddc531355c7c24c59662434de78f6
CreeperPlanet26 <fortniteplanet26@gmail.com>
2020-09-07 15:24:41 -0400

    everytime a noti is made it will delete duplicates

--- nest/src/notification/notification.service.ts
+++ nest/src/notification/notification.service.ts
@@ -13,12 +13,17 @@ export class NotificationService {
         private events: EventsGateway
     ) { }
 
+    private async rmDuplicates(user: User): Promise<User> {
+        user.notifications = [...new Set(user.notifications)];
+        return await user.save();
+    }
+
     public async handleBotApprove(bot: Bot): Promise<User> {
         for (const id of bot.owners) {
             const user = await this.Users.findOne({ id });
             user.notifications.push({ message: `Your bot, ${bot.tag}, was approved. 🤖`, read: false });
             this.events.emitNewNotification(user);
-            return await user.save();
+            return this.rmDuplicates(user);
         }
     }
 
@@ -27,7 +32,7 @@ export class NotificationService {
             const user = await this.Users.findOne({ id });
             user.notifications.push({ message: `Your bot, ${bot.tag}, was rejected. 😢`, read: false });
             this.events.emitNewNotification(user);
-            return await user.save();
+            return this.rmDuplicates(user);
         }
     }
 
@@ -37,7 +42,7 @@ export class NotificationService {
             ownerObject.notifications.push({ message: `${reviewer.tag} just rated your bot ${rating} stars! ⭐`, read: false });
             this.events.emitNewNotification(ownerObject);
             try {
-                return await ownerObject.save();
+                return await this.rmDuplicates(ownerObject);
             } catch (err) {
                 return new HttpException("Something went wrong and the owners were not notified of the reviews.", HttpStatus.INTERNAL_SERVER_ERROR);
             }
@@ -47,12 +52,12 @@ export class NotificationService {
     public async handleReviewLike(reviewOwner: User, liker: User): Promise<User | HttpException> {
         reviewOwner.notifications.push({ message: `${liker.tag} liked your review! 👍`, read: false });
         this.events.emitNewNotification(reviewOwner);
-        return await reviewOwner.save();
+        return await this.rmDuplicates(reviewOwner);
     }
 
     public async handleReviewDislike(reviewOwner: User, disliker: User): Promise<User | HttpException> {
         reviewOwner.notifications.push({ message: `${disliker.tag} disliked your review 👎.`, read: false });
         this.events.emitNewNotification(reviewOwner);
-        return await reviewOwner.save();
+        return await this.rmDuplicates(reviewOwner);
     }
 }
