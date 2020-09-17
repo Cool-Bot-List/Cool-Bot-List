e8301b8023251936b9ab7acfba7638cedc6191e4
Zyleaf <Zyleaff@gmail.com>
2020-09-17 14:10:36 -0400

    Redux setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,38 @@
-import React from "react";
-import "./App.css";
+import "./styles/index.css";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { SET_USER } from './redux/actions/user.actions';
+import { UserType } from './types/graphql';
 
-function App() {
-    return <h1>hi</h1>;
+interface Props {
+    SET_USER: Function;
+    userReducer: UserType;
 }
 
-export default App;
+class App extends Component<Props> {
+    componentDidMount() {
+        this.props.SET_USER();
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Bio: {this.props.userReducer.bio}</h1>
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = (state: any) => {
+    return ({
+        userReducer: state.userReducer
+    });
+};
+
+const mapDispatchToProps = (dispatch: Function) => {
+    return ({
+        SET_USER: () => dispatch(SET_USER()),
+    });
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
