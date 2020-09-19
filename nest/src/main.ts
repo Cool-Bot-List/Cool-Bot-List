a3594819091123a719018e045b42680f9447c5b5
CreeperPlanet26 <fortniteplanet26@gmail.com>
2020-09-19 17:28:09 -0400

    fix gh workflow?

--- nest/src/main.ts
+++ nest/src/main.ts
@@ -3,7 +3,7 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import * as session from "express-session";
 import * as passport from "passport";
-
+console.log(process.env);
 async function bootstrap(): Promise<void> {
     const app = await NestFactory.create(AppModule);
     app.setGlobalPrefix("api");
